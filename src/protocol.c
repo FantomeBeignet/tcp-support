19319201344ad39b4bfd05295a020a430b758536
Tom Béné <himself@fantomebeig.net>
2023-04-30 03:29:58 +0200

    feat: functions to pack and unpack messages

--- src/protocol.c
+++ src/protocol.c
@@ -10,14 +10,46 @@ uint16_t unpack_uint16(char *buffer) {
   return ((uint16_t)buffer[0] << 8) | buffer[1];
 }
 
-int rightpad(char *buffer, int l) {
-  size_t buflen = strlen(buffer);
-  if (buflen > l)
-    return -1;
-  if (buflen == l)
-    return 0;
-  for (int i = buflen; i < l; i++) {
-    buffer[i] = '\0';
+char *rightpad(const char *s, int l) {
+  size_t slen = strlen(s);
+  if (slen > l)
+    return NULL;
+  char *ret = calloc(l, sizeof(char));
+  memcpy(ret, s, slen);
+  for (int i = slen; i < l; i++) {
+    ret[i] = '\0';
   }
-  return (l - buflen);
+  return ret;
+}
+
+void pack_msg(char *buffer, const char *msg_type, const char *sender_uname,
+              const char *content) {
+  const char *padded_uname = rightpad(sender_uname, 32);
+  if (!padded_uname)
+    perror("Error padding username");
+  size_t msglen = strlen(content) + 1;
+  pack_uint16(buffer, msglen);
+  memcpy(buffer + 16, msg_type, 4);
+  memcpy(buffer + 20, padded_uname, 32);
+  memcpy(buffer + 52, content, strlen(content));
+  buffer[msglen - 1] = '\0';
+}
+
+proto_msg *unpack_msg(char *buffer, uint16_t msg_size) {
+  proto_msg *ret = calloc(1, sizeof(proto_msg));
+  if (!ret)
+    perror("Could not allocate space to unpack message");
+  char *msg_type = calloc(4, sizeof(char));
+  char *sender_uname = calloc(32, sizeof(char));
+  char *content = calloc(msg_size, sizeof(char));
+
+  strncpy(msg_type, buffer, 4);
+  strncpy(sender_uname, buffer + 4, 32);
+  strncpy(content, buffer + 36, msg_size);
+
+  ret->msg_type = msg_type;
+  ret->sender_uname = sender_uname;
+  ret->content = content;
+
+  return ret;
 }
--- src/protocol.h
+++ src/protocol.h
@@ -2,8 +2,19 @@
 #define __PROTOCOL_H__
 
 #include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+typedef struct proto_msg {
+  char *msg_type;
+  char *sender_uname;
+  char *content;
+} proto_msg;
+
+void pack_msg(char *buffer, const char *msg_type, const char *sender_uname,
+              const char *content);
+proto_msg *unpack_msg(char *buffer, uint16_t msg_size);
 
 #endif
