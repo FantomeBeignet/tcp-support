096b783b0a26cb89bbef0982b3b65ac5e119f701
Tom Béné <himself@fantomebeig.net>
2023-05-04 14:18:43 +0200

    fix: username input

--- src/client/client.c
+++ src/client/client.c
@@ -27,22 +27,20 @@ int main(int argc, char *argv[]) {
 
   serv_addr.sin_port = htons(serv_port);
   serv_addr.sin_addr.s_addr = inet_addr(serv_adress);
+  char username[32];
   if (connect(clientSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) <
       0) {
     perror("Erreur à la connexion");
     exit(1);
   } else {
-    char username[32];
-    int k;
     printf("Entrez un nom d'utilisateur : ");
-    if ((k = recv(clientSocket, username, sizeof(username), 0)) < 0) {
-      perror("Pas de nom d'utilisateur saisi");
-      exit(1); // Force la sortie pour le moment, peut-être à modifier plus tard
+    if (!fgets(username, 32, stdin)) {
+      perror("Veuillez rentrer un nom d'utilisateur.");
+      exit(1);
     }
-    username[k] = '\0';
-    fgets(username, 32, stdin);
-    if (send(clientSocket, username, strlen(username), 0) < 0) {
-      perror("Erreur à l'envoi du nom d'utilisateur");
+    username[strcspn(username, "\r\n")] = 0;
+    if (!strcmp(username, "\0")) {
+      perror("Veuillez rentrer un nom d'utilisateur.");
       exit(1);
     }
   }
