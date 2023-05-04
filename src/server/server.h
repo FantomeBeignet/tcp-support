2b78a36c2c647a463b7e237a06699b0f6ac7eb98
Tom Béné <himself@fantomebeig.net>
2023-05-04 12:26:13 +0200

    feat: base for server

---
+++ src/server/server.h
@@ -0,0 +1,16 @@
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#endif // !__SERVER_H__
+
